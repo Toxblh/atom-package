3a6867c1ffc3e5389a2f3e72a7eebe32bf1dc3c3
Anton Palgunov <toxblh@gmail.com>
2016-09-16 13:23:53 +0300

    change name package > package-list

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Backup package list'
-apm list --installed --bare | grep '(.*)@' -Eo | cut -d@ -f1 > package.txt
+apm list --installed --bare | grep '(.*)@' -Eo | cut -d@ -f1 > package-list.txtt
 echo 'Backup settings atom'
 cp ~/.atom/config.cson .
 echo 'Succesful!'
