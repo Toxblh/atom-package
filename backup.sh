46c2a3074b5507856eccfef4b8b86c9cac3efbd7
Anton Palgunov <toxblh@gmail.com>
2016-12-28 07:38:31 +0300

    add styles config

--- backup.sh
+++ backup.sh
@@ -4,4 +4,6 @@ echo 'Backup package list'
 apm list --installed --bare | grep '\(.*\)@' -o | sed -e 's/@//g' > package-list.txt
 echo 'Backup settings atom'
 cp ~/.atom/config.cson .
+echo 'Backup styles atom'
+cp ~/.atom/styles.less .
 echo 'Succesful!'
--- restore.sh
+++ restore.sh
@@ -2,4 +2,6 @@ echo 'Start restore atom'
 apm install --packages-file package-list.txt
 echo 'Restore settings'
 cp ./config.cson ~/.atom
+echo 'Restore styles'
+cp ./styles.less ~/.atom
 echo 'Succesful!'
